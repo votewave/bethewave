4a2a396be0e2f939d5e069a49df08959812ce1bb
Rob Lanphier <robla@robla.net>
2017-12-10 15:14:28 -0800

    Added some very crude mockup screens

--- bethewave-server.py
+++ bethewave-server.py
@@ -6,5 +6,24 @@ app = Flask(__name__)
 def main():
     return render_template('index.html')
 
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
+
+@app.route('/dashboard-dec10')
+def dashboard1():
+    return render_template('dashboard-dec10.html')
+
+@app.route('/dashboard-dec17')
+def dashboard2():
+    return render_template('dashboard-dec17.html')
+
+@app.route('/dashboard-jan03')
+def dashboard3():
+    return render_template('dashboard-jan03.html')
+
+
 if __name__ == "__main__":
     app.run()
